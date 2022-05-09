c5e8cc4cd73de458de525c832792e96005397cf8
bushmrz <buuushmrz@mail.ru>
2022-05-09 12:33:51 +0300

    .

--- W_Gen/Program.cs
+++ W_Gen/Program.cs
@@ -1,12 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
 using Xceed.Words.NET;
-using Word = Microsoft.Office.Interop.Word;
-using System.IO;
 
 namespace W_Gen
 {
